6b370dce7fe3b7e752bae154870f4ce676bbf3d1
Mohammad Ali Kassem Hijazi <mohammad_hejazii@outlook.com>
2025-01-25 16:19:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
-
+import Head from 'next/head';
 import EventList from '../Components/events/event-list';
 import { getFeaturedEvents } from '../dummy-data';
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
-  return (
-   
-      <EventList items={featuredEvents}></EventList>
 
+  return (
+    <>
+      <Head>
+  
+        <title>Featured Events - Event Management App</title>
+        <meta
+          name="description"
+          content="Check out the latest featured events on our platform."
+        />
+        <meta name="keywords" content="featured events, latest events" />
+      </Head>
+      <EventList items={featuredEvents} />
+    </>
   );
 }
 
